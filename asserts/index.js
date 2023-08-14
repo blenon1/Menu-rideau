f7710be940411fe60331a479b363d13734396dcf
blenon1 <williamblenon@gmail.com>
2023-08-14 15:55:17 +0200

    premier depot

---
+++ asserts/index.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector(".nav-toggler")
+const navigation = document.querySelector("nav")
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active")
+})
